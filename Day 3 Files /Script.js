9f4ec5407fb74a4196377b49c54e4e7a32d7e78a
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-09 15:15:38 -0400

    Day 3 Examples
    Created two web pages and two java script files

---
+++ Day 3 Files /Script.js
@@ -0,0 +1,41 @@
+/**
+ * Created by AndrewSorensen on 5/9/14.
+ */
+//alert("Hello World!");
+
+// Comment out one line only
+
+/*
+Multiple
+Line
+Commenting
+ */
+
+// ALerts create a pop up box for the user
+// You must hit OK to make it go away
+//alert("This is an alert");
+
+//Console is used for designers to check values
+//Print out values for us to see and no one else
+console.log("This is the console.");
+
+//Declare a variable
+// Use keyword var
+
+var variableName; // Declaration
+variableName=42; //Definition
+// Storage container= Value inside of it
+//Can Declare and Define in one step
+var a = 2;
+
+//Print out the variable "a" into our console
+console.log(a);
+console.log("a");
+
+//Simple Math
+a = a+3;
+console.log(a)
+
+var b;
+b = a+3;
+console.log(b);
