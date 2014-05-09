f8babb3dca22e46b5774c82fa932b46ffbe38ed6
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-09 18:15:14 -0400

    Givens
    Givens for problem 1

--- Day 3 Lab Expressions /js.js
+++ Day 3 Lab Expressions /js.js
@@ -3,3 +3,11 @@ Andrew Sorensen
 Web Programming Fundamentals 1405
 Day 3 Lab Expressions
  */
+
+// Calculate how much pizza each party goer will receive
+
+//Givens
+var sliceNumber = 10;
+var guestNumber = 25;
+var pizzaNumber = 4;
+
