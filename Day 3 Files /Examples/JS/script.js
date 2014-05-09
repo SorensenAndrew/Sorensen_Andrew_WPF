1d326a12d40ac41bdfe68c539b85193df7924120
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-09 15:32:42 -0400

    Area of Triangle
    Found area of Triangle through use of variables

--- Day 3 Files /Examples/JS/script.js
+++ Day 3 Files /Examples/JS/script.js
@@ -13,3 +13,14 @@ var birthYear = 1989;
 //Create a variable to hold the age
 var age = 2014 - birthYear-1;
 console.log(age);
+
+//Math
+// + - / *
+
+//Find the area of a Triangle
+// base * height * .5
+var base = 8;
+var height = 5;
+
+var areaTriangle = base * height * .5;
+console.log(areaTriangle);
