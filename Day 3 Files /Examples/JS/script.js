9a1dc25e80105d437aa6316fefa22efb550b050e
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-09 16:02:16 -0400

    Assignment Operators
    Learned about the various assignment operators

--- Day 3 Files /Examples/JS/script.js
+++ Day 3 Files /Examples/JS/script.js
@@ -27,12 +27,30 @@ console.log(areaTriangle);
 
 //Modulo - %
 // Give the remainder
-
 var remainder = 32%10;
 console.log(remainder);
 
 // Find out if things are even or odd
-
 var evenOrOdd = 247673%2;
 console.log(evenOrOdd);
+
+//Assignment Operators
+/*
+=Assignment
+++ Adds 1 to the variable
+-- Subtracts 1 from the variable
++= Addition assignment
+-= Subtract assignment
+/= Division Assignment
+*= Multiplication assignment
+ */
+var a = 1;
+a++; //a = a+1
+a--; //a = a-1
+a+= 4; // a = a+4
+a-= 3;
+a/= 2;
+a*=6;
+console.log(a);
+
 
