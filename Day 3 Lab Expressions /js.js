2b1828fa4182fe46d68c4f37666e201dcf5ffe42
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-09 18:17:23 -0400

    Formula for problem 1
    Formula showing how many slices of pizza each guest receives.

--- Day 3 Lab Expressions /js.js
+++ Day 3 Lab Expressions /js.js
@@ -11,3 +11,6 @@ var sliceNumber = 10;
 var guestNumber = 25;
 var pizzaNumber = 4;
 
+// Formula depicting number of slices per person
+var slicePerGuest = sliceNumber * pizzaNumber / guestNumber;
+console.log(slicePerGuest);
