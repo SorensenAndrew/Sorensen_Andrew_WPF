fc61c21d5642b7627af86be2a91bb791a35b0811
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-12 19:17:43 -0400

    Expressions: Wacky- Calculation
    Expressed the array in a calculation to determine average.

--- Expressions- Wacky /Wacky.js
+++ Expressions- Wacky /Wacky.js
@@ -8,9 +8,14 @@
 
 //What is the average number of lemmings that walk to their deaths per day?
 
-var lemmingsDay1= prompt("How many lemmings died on day 1?");
-var lemmingsDay2= prompt("How many lemmings died on day 2?");
-var lemmingsDay3= prompt("How many lemmings died on day 3?");
+var lemmingsDay1= parseInt(prompt("How many lemmings died on day 1?"));
+var lemmingsDay2= parseInt(prompt("How many lemmings died on day 2?"));
+var lemmingsDay3= parseInt(prompt("How many lemmings died on day 3?"));
 
-var lemmingsDead= [lemmingsDay1, lemmingsDay2, lemmingsDay3]
+var lemmingsDead= [lemmingsDay1, lemmingsDay2, lemmingsDay3];
+console.log(lemmingsDead);
+
+var averageDead= (lemmingsDead[0] + lemmingsDead[1] + lemmingsDead[2]) / 3;
+console.log(averageDead);
+
 
