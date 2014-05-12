82d9de8edf8d5524de48b324b0a713c637c263ee
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-12 19:09:07 -0400

    Expressions: Wacky- Givens array
    Put all givens from previous step into an array together

--- Expressions- Wacky /Wacky.js
+++ Expressions- Wacky /Wacky.js
@@ -12,5 +12,5 @@ var lemmingsDay1= prompt("How many lemmings died on day 1?");
 var lemmingsDay2= prompt("How many lemmings died on day 2?");
 var lemmingsDay3= prompt("How many lemmings died on day 3?");
 
-
+var lemmingsDead= [lemmingsDay1, lemmingsDay2, lemmingsDay3]
 
