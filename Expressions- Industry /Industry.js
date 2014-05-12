c4d3eb4744a9b81c008b1bb7c41db10b47351484
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-12 18:45:52 -0400

    Expressions: Industry- Givens
    Created a list of given variables for the calculation

--- Expressions- Industry /Industry.js
+++ Expressions- Industry /Industry.js
@@ -6,4 +6,11 @@ Andrew Sorensen
  */
 //alert("Hello.");
 
-// How much money did I make in commission for the month? (Video rental store)
+// How much money did I make at work for the month? (Video rental store)
+
+var halfOffCard = prompt("How many half off cards did I sell?");
+var candyBundle= prompt("How many candy bundles did I sell?")
+var rent3Get1= prompt("How many rent 3 get 1 free bundles did I sell")
+var hourlyWage= 8.40
+var daysWorked= prompt("How many days did I work?")
+
