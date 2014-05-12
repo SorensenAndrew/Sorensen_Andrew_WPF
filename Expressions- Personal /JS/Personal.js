66098841794dbe63c25f79038a6249d64457d947
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-12 19:26:30 -0400

    Expressions: Personal- Comments
    Commenting lines of code

--- Expressions- Personal /JS/Personal.js
+++ Expressions- Personal /JS/Personal.js
@@ -7,15 +7,17 @@ Andrew Sorensen
 //alert("Hello");
 
 // How much time will it take to prepare for and arrive at school?
-
+//Creating variables for the prompts
 var eating= prompt("How long does it take you to make and eat food?");
 var hygeine= prompt("How long does it take you to shower, shave, and brush your teeth?");
 var driveTime = prompt("How long does it take for you to drive to school?");
-
+//adding the variables into a calculation for common variable, adding parseInt to get numeric value from answers
 var prepTime= parseInt(eating) + parseInt(hygeine) + parseInt(driveTime);
+//Testing calculation
 console.log(prepTime);
-
+//Put calculation into final expression
 alert("It takes you " + prepTime + " minutes to get prepared for and arrive at school." );
+//Testing expression 
 console.log("It takes you " + prepTime + " minutes to get prepared for and arrive at school." );
 
 
