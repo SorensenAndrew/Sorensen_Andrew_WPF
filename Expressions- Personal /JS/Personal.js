23dcde5d388cf534f9abd9209614b2959001c06e
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-12 18:18:15 -0400

    Expressions-Personal Givens
    Added calculation question and given variables to prompt with

--- Expressions- Personal /JS/Personal.js
+++ Expressions- Personal /JS/Personal.js
@@ -2,6 +2,13 @@
 Andrew Sorensen
  WPF-1
  Expressions- Personal
+ 5/12/14
  */
 //alert("Hello");
+
+// How much time will it take to prepare for and arrive at school?
+
+var eating= prompt("How long does it take you to make and eat food?");
+var hygeine= prompt("How long does it take you to shower, shave, and brush your teeth?");
+var driveTime = prompt("How long does it take for you to drive to school?");
 
