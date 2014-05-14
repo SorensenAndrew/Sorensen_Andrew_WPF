fd4cc42a7e698c2edb035bd85524eb8edef2041b
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-14 16:30:11 -0400

    Ternary- Basic IF statement
    Starting file

---
+++ Day 5 Files/JS/Day 5_Ternary.js
@@ -0,0 +1,18 @@
+/**
+ Andrew Sorensen
+ WPF-1
+ Ternary
+ */
+//alert("Test");
+
+//Check our GPA and see if we can graduate
+//Higher than 2.0 GPA to graduate
+
+var gpa= 3.7
+var minGPA= 2.0
+
+if(gpa>minGPA){
+    console.log("You can graduate.:)");
+} else {
+    console.log("You cannot graduate. :(");
+}
