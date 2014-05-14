69134fedca42e754c3e3cb10d4c2523fd84db805
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-14 16:20:34 -0400

    OR ||
    1 or more of the sides must be true, to gain true result

--- Day 5 Files/JS/Day 5_Logical.js
+++ Day 5 Files/JS/Day 5_Logical.js
@@ -18,3 +18,19 @@ if(iPadPrice<=budget && paycheck>600){
 } else{
     console.log("You cannot buy the iPad.");
 }
+
+//Can we buy a car?
+//If the price of the car is less than our budget- buy the car
+// OR if you win the lottery- buy the car
+var carPrice = 35000;
+var carBudget= 12000;
+var winLottery= true
+
+//OR || one or more must be true to gain true result
+
+if(carPrice<carBudget|| winLottery){
+    console.log("Buy the car.");
+} else {
+    console.log("Don't buy the car.");
+}
+
