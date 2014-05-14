6b0533be7b60aa2b69a1fd8481133c58925712e6
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-14 16:45:54 -0400

    Ternary Variable
    Set value using Ternary

---
+++ Day 5 Files/JS/Day5_Ternary.js
@@ -0,0 +1,40 @@
+/**
+ Andrew Sorensen
+ WPF-1
+ Ternary
+ */
+//alert("Test");
+
+//Check our GPA and see if we can graduate
+//Higher than 2.0 GPA to graduate
+
+var gpa= 3.7
+var minGPA= 2.0
+
+if(gpa>minGPA){
+    console.log("You can graduate.:)");
+} else {
+    console.log("You cannot graduate. :(");
+}
+
+//(Condition to be met)? True code: False code;
+
+(gpa>2.0)? console.log("You graduate :)"):console.log("You cannot graduate :(");
+
+//Pick a book based on a kids age
+//Under 10- Green eggs and ham, 10+ gets Time Machine
+
+var age=6
+
+(age < 10)? console.log("Green eggs and ham") : console.log("Time Machine")
+(age>=10)? console.log("Time Machine"): console.log("Green Eggs and Ham")
+
+//Save the book name to a variable
+var book;
+
+book= (age<10)? "Green Eggs and Ham.":"Time Machine"
+
+if(age<10){
+    book= "Time Machine";
+}
+console.log("This is the statement for if")
