0b39e01d0fe917e59abb6cf3128ee2b098b1a50a
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-16 18:26:08 -0400

    Wacky Conditional
    Created html and js files, linked and tested

--- Conditionals_Industry/Industry.js
+++ Conditionals_Industry/Industry.js
@@ -13,5 +13,8 @@ Andrew Sorensen
 var age= prompt("How old is the customer?");
 var money= prompt("How much money does the customer have?");
 
+//Set variables to a ternary statement, allows the prompt responses to determine result
+
+ (age>= 17 && money>= 2.80)? console.log("Yes, the customer can rent the movie."):console.log("No, the customer cannot rent the movie.")
 
 
---
+++ Conditionals_Wacky/Wacky.js
@@ -0,0 +1,6 @@
+/**
+ Andrew Sorensen
+ WPF-1
+ Wacky Conditional
+ */
+//alert("Test");
