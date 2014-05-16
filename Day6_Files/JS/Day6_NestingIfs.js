9e87309fb99100de996524933e60d2c61d4d4bde
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-16 14:52:33 -0400

    Day 6 Nesting Ifs
    Created files, created incomplete setup for calculations

---
+++ Day6_Files/JS/Day6_NestingIfs.js
@@ -0,0 +1,32 @@
+/**
+Andrew Sorensen
+ WPF-1
+ Nesting Ifs
+ */
+
+//alert("Test");
+
+//Some decisions affect other decisions
+
+//If it is warm outside, lets go to the beach
+//If its not, lets go to the movies
+
+//If the water is warm, then Im going in
+//If it is cold, I am going to get a tan
+
+//Create variable for outside temp
+
+var temp= 84; //degrees
+var waterTemp= 78;
+
+if(temp>=85){
+    console.log("Lets go to the beach!");
+}else{
+    console.log("Lets go to the movies!");
+}
+
+if(waterTemp>= 75){
+    console.log("Lets get in the water.");
+}else{
+    console.log("Lets get a tan.");
+}
