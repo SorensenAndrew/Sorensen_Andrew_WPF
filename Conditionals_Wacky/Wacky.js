37ea39f02215daf592f534728bd618e10daa6c15
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-16 19:03:35 -0400

    Wacky Conditional
    Final result, put into an expression

--- Conditionals_Wacky/Wacky.js
+++ Conditionals_Wacky/Wacky.js
@@ -15,11 +15,13 @@ var fedoraCirc= prompt("What is the circumference of the fedora?");
 var perryThePlatypus= "Can the platypus wear the fedora?"
 
 //Statement that uses user prompted variables and calculates them to find the result
-if(platpusHead*Math.PI === fedoraCirc){
+if(platpusHead*Math.PI.toFixed(1) == fedoraCirc){
     console.log("The platypus can wear the fedora.")
     //Set final result variable to each possible outcome
-    perryThePlatypus= "Since the cicumference of the hat matches that of the platypus, the platypus can wear the fedora."
+    perryThePlatypus= "Since " + platpusHead*Math.PI.toFixed(1) +", the circumference of the platypus's head matches " + fedoraCirc + ", the circumference of the fedora, the platypus can wear the hat.";
 }else{
     console.log("The platypus cannot wear the fedora.")
-    perryThePlatypus= "Since the circumference of the hat does not match the circumference of the platypus, the platypus cannot wear the fedora."
+    perryThePlatypus=  perryThePlatypus= "Since " + platpusHead*Math.PI.toFixed(1) +", the circumference of the platypus's head does not match " + fedoraCirc + ", the circumference of the fedora, the platypus can wear the hat.";
 }
+//final result variable logged to the console to show final expression
+console.log(perryThePlatypus);
