44cd7a0780960d40a4d0a35a5d7bc82ed3511ffd
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-16 15:23:44 -0400

    Day 6 Math
    Rounding using Math.(ceil, floor, round)

---
+++ Day6_Files/JS/Day6_Math.js
@@ -0,0 +1,28 @@
+/**
+Andrew Sorensen
+ WPF-1
+ Math
+ */
+//alert("test");
+
+//Math.round()
+//Normal rounding .5 goes up, less goes down
+
+var num1= 9.5444;
+console.log(num1);
+console.log(Math.round(num1));
+
+//Math.floor()
+//Removes all numbers past decimal
+
+var num2= 6.9;
+console.log(num2);
+console.log(Math.floor(num2));
+
+//Math.ceil()
+//Rounds number up to next integer
+
+var num3= 4.00000001;
+console.log(num3);
+console.log(Math.ceil(num3));
+
