b6691604db150304e6ef6ef5345ed680ca722d40
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-16 18:47:33 -0400

    Wacky Conditionals
    Set the variables

--- Conditionals_Wacky/Wacky.js
+++ Conditionals_Wacky/Wacky.js
@@ -9,3 +9,7 @@
 //Creating the calculation
 // Is the platypus's (platypi?) head large enough to wear a fedora?
 
+//Variables that will be entered by the user, which will determine the result of the calculation
+var platpusHead= prompt("What is the diameter of the platypus's head?");
+var fedoraCirc= prompt("What is the circumference of the fedora?");
+
