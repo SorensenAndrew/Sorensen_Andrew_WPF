33ec290f938637533b61fca0f17ab282f322dfe5
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-16 18:11:30 -0400

    Industry Conditionals
    Set variables to be determined by user prompt

--- Conditionals_Industry/Industry.js
+++ Conditionals_Industry/Industry.js
@@ -9,3 +9,8 @@ Andrew Sorensen
 //Creating problem to be calculated
 // What is the customers total bill for all movie rentals?
 
+//Variables, to be determined by user prompt
+var newReleases= prompt("How many new releases did the customer choose?");
+var dollarMovies= prompt("How many dollar movies did the customer choose?");
+var freeMovies= prompt("Is it Tuesday, Wednesday, or Thursday?");
+
