25b2ed1012e24929a3bbfb87d4b64c445294481b
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-16 18:42:35 -0400

    Wacky Conditional
    Set up the problem for calculation

--- Conditionals_Wacky/Wacky.js
+++ Conditionals_Wacky/Wacky.js
@@ -4,3 +4,8 @@
  Wacky Conditional
  */
 //alert("Test");
+// You told us not to worry about validating for this assignment
+
+//Creating the calculation
+// Is the platypus's (platypi?) head large enough to wear a fedora?
+
