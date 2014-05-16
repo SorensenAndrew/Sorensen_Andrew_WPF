3572e960f38621e7d7bbda70b45e8c9e8768a403
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-16 18:16:31 -0400

    Industry Conditionals
    Restarted calculation with new problem

--- Conditionals_Industry/Industry.js
+++ Conditionals_Industry/Industry.js
@@ -7,10 +7,5 @@ Andrew Sorensen
 //You told us to not worry about validating for this assignment
 
 //Creating problem to be calculated
-// What is the customers total bill for all movie rentals?
-
-//Variables, to be determined by user prompt
-var newReleases= prompt("How many new releases did the customer choose?");
-var dollarMovies= prompt("How many dollar movies did the customer choose?");
-var freeMovies= prompt("Is it Tuesday, Wednesday, or Thursday?");
+// Can the customer rent an R rated film?
 
