618d6de218eb3404ada658eeef4b1a05054cb8e2
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-16 18:07:36 -0400

    Industry Conditionals
    Created problem to be calculated

--- Conditionals_Industry/Industry.js
+++ Conditionals_Industry/Industry.js
@@ -4,3 +4,8 @@ Andrew Sorensen
  Industry Conditionals
  */
 //alert("Test");
+//You told us to not worry about validating for this assignment
+
+//Creating problem to be calculated
+// What is the customers total bill for all movie rentals?
+
