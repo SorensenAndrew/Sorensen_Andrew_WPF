407505eb8d2c36cc2fbba0fb29325498196e50bf
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-19 19:50:43 -0400

    Loops Assignment-Final
    Final edits made to code, submitted

--- Loops_worksheet/JS/Do_While_Loops.js
+++ Loops_worksheet/JS/Do_While_Loops.js
@@ -6,18 +6,19 @@
 
 //Calculating how fast a Delorian accelerates before traveling back in time
 //Variable that is altered by the do/while statement
-mph= prompt("How fast is the Delorian going?");
+mph= prompt("How fast is the Delorean going?");
 while(isNaN(mph) || mph===""){
-    mph= prompt("How fast is the Delorian going?");
+    mph= prompt("How fast is the Delorean going?");
 }
 //Runs code first
 do{
-    console.log("The Delorian is going "+mph+"mph.");
+    console.log("The Delorean is going "+mph+"mph.");
     //Doesnt run until second run of code
     mph++
     //Dictates when code stops running
 }while(mph<88);
 //What happens after the first set of code becomes false
 if(mph===88){
-    console.log("The Delorian has traveled back in time.");
+    console.log("The Delorean has traveled back in time.");
 }
+//DeLorean
