562b609e897a7088cbbc3fd43efe5600d75e093e
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-19 14:16:37 -0400

    Do/While Example
    Do/while loop runs at least once before testing the conditional

--- Day7 Files/JS/While_Loops.js
+++ Day7 Files/JS/While_Loops.js
@@ -19,3 +19,15 @@ while(counter<200){ //condition loops until false
 }
 
 console.log("Ending counter " +counter)
+
+//Do While Loop
+//Runs code once to start before checking if the condition is true, then repeats as long as it is true
+
+//Initialize the variable
+var i=20;
+//Set up do/while loop
+do{
+    console.log("The number value of i is "+i);
+    i++
+} while(i<10);//Condition to test
+console.log(i);
