ee86cb1678ae89ce5fe5e0e35e20af832eed76f9
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-19 18:22:36 -0400

    Loops assignment
    Saving progress, broke browser with code

--- Loops_worksheet/JS/While_Loops.js
+++ Loops_worksheet/JS/While_Loops.js
@@ -4,3 +4,13 @@
  * Loops Worksheet
  */
 
+//What is the user's date of birth?
+
+var month= String(prompt("What is the month of your birth?(Written in letters)"));
+//
+//var year= Number(prompt("What is the year of your birth?"));
+
+while(month==="") {
+    month = String(prompt("What is the month of your birth?(Written in letters.)"));
+}
+//var date= Number(prompt("What is the date of your birth?"));
