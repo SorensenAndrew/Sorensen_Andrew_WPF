3aec372dedb647f8425322137fae347c52c7ae6a
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-19 19:05:59 -0400

    Loops worksheet-While
    Commented and finalized

--- Loops_worksheet/JS/While_Loops.js
+++ Loops_worksheet/JS/While_Loops.js
@@ -4,13 +4,25 @@
  * Loops Worksheet
  */
 
-//What is the user's date of birth?
-
-var month= String(prompt("What is the month of your birth?(Written in letters)"));
-//
-//var year= Number(prompt("What is the year of your birth?"));
-
-while(month==="") {
-    month = String(prompt("What is the month of your birth?(Written in letters.)"));
+//What is the user's birthday?
+//Variables determined by prompts
+var month= prompt("What is the month of your birth?(Written in letters)");
+//Check prompt using while statement
+while(month==="" || month>= 0){
+    month= prompt("What is the month of your birth?(Written in letters)");
 }
-//var date= Number(prompt("What is the date of your birth?"));
+console.log(month);
+var date= prompt("What is the date of your birth?");
+while(isNaN(date) || date===""){
+    date= prompt("What is the date of your birth?");
+}
+console.log(date);
+var year= prompt("What is your year of birth?");
+while(isNaN(year) || year===""){
+    year= prompt("What is your year of birth?");
+}
+console.log(year);
+//Expression put into variable for final result 
+var dob= "Your date of birth is "+month+", "+date+"th "+year;
+
+console.log(dob);
