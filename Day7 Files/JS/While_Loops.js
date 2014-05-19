ddd40a345f632e04b1689d6b4d0d7f7be2e5c82c
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-19 14:05:07 -0400

    Basic While Loop
    Can potentially never end…

--- Day7 Files/JS/While_Loops.js
+++ Day7 Files/JS/While_Loops.js
@@ -5,4 +5,17 @@ Andrew Sorensen
  */
 //alert("This is a test, this is only a test.");
 
+//Basic while loop
+//While loop loops through a block of code as long as the condition is true
+//Initialize the counter variable
 
+var counter= 0;
+//Set up while loop
+while(counter<200){ //condition loops until false
+    console.log("Some things just never end.");
+    //Change the value of the counter variable or it will run forever
+    console.log("The counter is " + +counter);
+    counter++
+}
+
+console.log("Ending counter " +counter)
