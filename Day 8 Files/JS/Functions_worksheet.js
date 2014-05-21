1f7a5ddcd0ce299d46edeabeb95dd34c00a1ad6c
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-21 18:16:18 -0400

    Function assignment
    Created calculation for circumference of a circle

--- Day 8 Files/JS/Functions_worksheet.js
+++ Day 8 Files/JS/Functions_worksheet.js
@@ -6,4 +6,18 @@
 
 //alert("Danger! Danger, Will Robinson!");
 
-//Calculate the circumference of a circle 
+//Calculate the circumference of a circle
+//Create function to house calculation
+function calcCirc (r){//r= radius, the parameter
+    //Variable that holds the calculation within the function
+    var circumferenceCalc= 2*Math.PI*r;
+    //creates the log of the calculation
+    console.log("The circumference of the circle is "+circumferenceCalc+".");
+    //allows the result to exist outside of the function
+    return circumferenceCalc;
+
+}
+var circleCirc= calcCirc(5);
+
+
+
