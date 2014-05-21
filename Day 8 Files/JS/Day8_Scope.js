17cb810c5be7187d7c376a8319b2a2a529e1f025
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-21 15:54:07 -0400

    Variable Scope
    Variables declared inside of a function stay there

--- Day 8 Files/JS/Day8_Scope.js
+++ Day 8 Files/JS/Day8_Scope.js
@@ -13,10 +13,15 @@
 //This is going to be impossible in large files
 
 //Create a variable for width in our main code
-var width=5;
+var width=5;//Scoped outside of the function
 
 //Create a function that calculates the perimeter of a rectangle
 function calcPeri(){
-    console.log(width);
+    var width= 10//Scoped to the function
+    var height= 20;
+    var perimeter=width*2+height*2
+    console.log("Inside of function, the perimeter is "+perimeter+".");
 }
 calcPeri();
+//console.log("After call the perimeter is "+perimeter); //Cannot access a variable declared inside a function
+
