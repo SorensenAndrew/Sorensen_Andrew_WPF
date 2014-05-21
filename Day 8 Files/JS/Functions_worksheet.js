57026ae20b49ea9f7995125064469cd278dc29b3
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-21 18:26:20 -0400

    Function worksheet
    Created 2nd calculation for assignment

--- Day 8 Files/JS/Functions_worksheet.js
+++ Day 8 Files/JS/Functions_worksheet.js
@@ -11,13 +11,28 @@
 function calcCirc (r){//r= radius, the parameter
     //Variable that holds the calculation within the function
     var circumferenceCalc= 2*Math.PI*r;
-    //creates the log of the calculation
-    console.log("The circumference of the circle is "+circumferenceCalc+".");
     //allows the result to exist outside of the function
     return circumferenceCalc;
 
 }
+//Putting the function in a variable
 var circleCirc= calcCirc(5);
+//Using variable to print the result of the function
+console.log("The circumference of the circle is "+circleCirc+".");
 
+// Stung! How many bee stings is required to kill an animal?
+//Variable used for weight in the calculation
+var weight= 50;
+//Function to house calculation
+function beeStings(weight){
+    //Stings per pound calculation
+    var stingDeath= weight*8.666666667
+    //bringing results out of the function
+    return stingDeath;
 
+}
+//Putting the function in a variable, argument set to original variable for weight
+var numberStings= beeStings(weight);
+//Log function result to console 
+console.log(numberStings);
 
