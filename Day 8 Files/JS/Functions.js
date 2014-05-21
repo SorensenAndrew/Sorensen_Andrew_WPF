a67b9eb00db5416817bdb5f57ffb9a4198957b30
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-21 15:21:06 -0400

    Created 3 functions
    Added a more complex example

--- Day 8 Files/JS/Functions.js
+++ Day 8 Files/JS/Functions.js
@@ -11,7 +11,24 @@ function printHello () {
     console.log("Hello");
 }
 
+//Create a function that logs "prints out more"
+function printMore(){
+    console.log("Prints out more text");
+}
 //Function Call our printHello
 //functionName();
 printHello();
+printMore();
+printHello();
+printMore();
 
+//Create a function that calculates the area of a rectangle
+
+function calcArea(){
+    //Create variables for width, height, and area
+    var width=20;
+    var height=30;
+    var area= width*height;
+    console.log(area);
+}
+calcArea();
