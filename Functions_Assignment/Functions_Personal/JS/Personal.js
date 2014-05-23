e06e113905af28e5aec32961e3ca657aea422260
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-23 18:08:59 -0400

    Functions-Personal
    Set up calculation and prompt variable

--- Functions_Assignment/Functions_Personal/JS/Personal.js
+++ Functions_Assignment/Functions_Personal/JS/Personal.js
@@ -4,3 +4,11 @@
  * Functions- Personal
  */
 //alert("Alert!");
+
+//What am I having for breakfast?
+
+var howHungry= prompt("How hungry am I? Answer with Very, Slightly, or Not at all.");
+while(howHungry!= "Very" && howHungry!="Slightly"&& howHungry!="Not at all"){
+    howHungry= prompt("How hungry am I? Answer with Very, Slightly, or Not at all.");
+}
+//console.log(howHungry);
