29a49c77dbd3541d79e2e71b603c0bc879d15842
SorensenAndrewWPF <pumaomega@fullsail.edu>
2014-05-23 19:59:29 -0400

    Functions Assignment
    Final Submission

--- Functions_Assignment/Functions_Wacky/JS/Wacky.js
+++ Functions_Assignment/Functions_Wacky/JS/Wacky.js
@@ -4,3 +4,19 @@
  * Assignment Name
  */
 //alert("Alert!");
+
+//Did you successfully kill the Nazi?
+var naziArmor=5;
+var bullets= prompt("How many bullets hit the nazi?");
+while(isNaN(bullets) || bullets===""){
+    bullets= prompt("How many bullets hit the nazi?");
+}
+function naziDeath(b){
+    shotThrough= b-naziArmor;
+    return shotThrough;
+}
+var finallyDead= naziDeath(bullets);
+(finallyDead<10)? console.log("The Nazi is not dead."):console.log("The Nazi is dead.");
+if(finallyDead>100){
+    console.log("He's really, really dead.");
+}
